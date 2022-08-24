83f81b49a836fd2334f657d1091d0ada7430f0d7
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-24 23:20:24 +0300

    Add update_likes_counter method to like class

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_likes_counter
+    post.update(LikesCounter: post.likes.count)
+  end
 end
