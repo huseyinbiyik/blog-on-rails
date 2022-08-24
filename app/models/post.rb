3dec7b2cb56ff5616d5777169aa6c8f2274b1293
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-24 22:17:26 +0300

    Add update_counter method to post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def update_counter
+    author.update(PostsCounter: author.posts.count)
+  end
 end
