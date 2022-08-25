094a26dd34866597d4c2d7af77928269c094531b
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-25 22:40:10 +0300

    Add update post counter spec to post class

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   subject do
-    u = User.new(name: 'Huseyin', photo: 'google.com', bio: 'I was born in 2055', posts_counter: '5')
-    Post.new(title: 'TestTitle', text: 'TestText', comments_counter: '0', likes_counter: '0', author: u)
+    u = User.new(name: 'Huseyin', photo: 'google.com', bio: 'I was born in 2055', posts_counter: '0')
+    Post.create(title: 'TestTitle', text: 'TestText', comments_counter: '0', likes_counter: '0', author: u)
+    Post.create(title: 'TestTitle', text: 'TestText', comments_counter: '0', likes_counter: '0', author: u)
   end
 
   before { subject.save }
@@ -36,5 +37,10 @@ RSpec.describe Post, type: :model do
   it 'likes counter should be equal to 0' do
     subject.likes_counter = 0
     expect(subject).to be_valid
+  end
+
+  it 'should update post counter' do
+    expect(subject.update_posts_counter).to eq true
+    expect(subject.author.posts_counter).to eq 2
   end
 end
