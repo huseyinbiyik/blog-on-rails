1abcae4b3a5f7a062bcb805fa7374f2dd3d90d4a
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-30 15:18:50 +0300

    Add tests for users/:id/posts/:id  contoller

--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -10,9 +10,24 @@ RSpec.describe 'Posts', type: :request do
       get '/users/1/posts'
       expect(response).to render_template('index')
     end
-    it 'renders the users list' do
+    it 'renders the posts as list' do
       get '/users/1/posts'
       expect(response.body).to include('List all posts of the specific user')
+    end
+  end
+
+  describe 'GET /users/:id/posts/:id' do
+    it 'is a HTTP success' do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders the show template' do
+      get '/users/1/posts/1'
+      expect(response).to render_template('show')
+    end
+    it 'renders the the post of specific user' do
+      get '/users/1/posts/1'
+      expect(response.body).to include('List specific post of the specific user')
     end
   end
 end
