a97515b06c7d8a5619c699aa1650850ad9ffa0b8
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-31 18:56:53 +0300

    Add user profile page view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @user_posts = @user.posts
+  end
 end
