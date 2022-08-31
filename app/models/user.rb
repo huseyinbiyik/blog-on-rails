c601e3d3fbdea5e0f5d76d0ae3c65cc5501423dc
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-31 23:32:34 +0300

    Change schema with the default value for posts_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { allow_blank: true, greater_than_or_equal_to: 0 }
 
   def three_recent_posts
     posts.includes(:author).order(created_at: :desc).limit(3)
