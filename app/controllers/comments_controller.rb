3147765d960611499704b74b03920376fdc06f6c
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-01 17:36:16 +0300

    Add update likes and comments counters after save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,11 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(post_id: params[:post_id], author_id: current_user.id, text: params[:text])
-    @comment.save
-    redirect_to '#'
+    if @comment.save
+      @comment.update_comments_counter
+      redirect_to '/'
+    else
+      flash[:error] = 'There is an error'
+    end
   end
 end
