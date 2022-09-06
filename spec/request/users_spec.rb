6a24c5e8ee376d0ea583a4b10d066f6a2e942043
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-06 14:36:21 +0300

    Fix controller tests for user and index pages

--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,33 +1,40 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Users', type: :request do
-#   describe 'GET #index' do
-#     it 'is a HTTP success' do
-#       get('/')
-#       expect(response).to have_http_status(:ok)
-#     end
-#     it "renders 'index' template" do
-#       get '/'
-#       expect(response).to render_template('index')
-#     end
-#     it 'renders the users list' do
-#       get '/'
-#       expect(response.body).to include('Here is the list of')
-#     end
-#   end
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before do
+      @first_user = User.create(name: 'Lewis', photo: 'https://www.formula1.com/content/fom-website/en/drivers/lewis-hamilton/_jcr_content/image.img.320.medium.jpg/1647334259839.jpg', bio: 'Driver from UK.')
+    end
+    it 'is a HTTP success' do
+      get('/')
+      expect(response).to have_http_status(:success)
+    end
+    it "renders 'index' template" do
+      get '/'
+      expect(response).to render_template('index')
+    end
+    it 'renders the users list' do
+      get '/'
+      expect(response.body).to include(@first_user.name)
+    end
+  end
 
-#   describe 'GET /users/:id' do
-#     it 'is a HTTP success' do
-#       get '/users/1'
-#       expect(response).to have_http_status(:success)
-#     end
-#     it 'renders the show template' do
-#       get '/users/1'
-#       expect(response).to render_template('show')
-#     end
-#     it 'renders the Post text' do
-#       get '/users/1'
-#       expect(response.body).to include('Show user profile')
-#     end
-#   end
-# end
+  describe 'GET /users/:user_id' do
+    before do
+      @first_user = User.create(name: 'Lewis', photo: 'https://www.formula1.com/content/fom-website/en/drivers/lewis-hamilton/_jcr_content/image.img.320.medium.jpg/1647334259839.jpg', bio: 'Driver from UK.')
+      @first_post = Post.create(author: @first_user, title: 'My first championship', text: "Mercedes Team Principal Toto Wolff has defended.\n")
+    end
+    it 'is a HTTP success' do
+      get "/users/#{@first_user.id}"
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders the show template' do
+      get "/users/#{@first_user.id}"
+      expect(response).to render_template('show')
+    end
+    it 'renders the Post text' do
+      get "/users/#{@first_user.id}"
+      expect(response.body).to include(@first_post.text)
+    end
+  end
+end
