ba71b3600cae170d5bb6c8a99b30088636f2c742
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-08 15:40:26 +0300

    Disable confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :comments, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
